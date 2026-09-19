edc0585026f27b2a2792cfbffc4ad72740b5cb57
Cherifb1 <115167632+Cherifb1@users.noreply.github.com>
2026-09-19 11:16:02 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+/* ====== INSTELLINGEN – pas hier aan ====== */
+const CONFIG = {
+  btwStandaard: 21,
+  vakantiegeldPct: 8,
+  prijsMaand: 9,                 // Premium per maand (€)
+  prijsJaar: 90,                 // Premium per jaar (€)
+  bedrijfKorting: 0.05,          // 5 % voor bedrijven die medewerkers uitbetalen
+  bedrijfsCode: "BEDRIJF5",      // kortingscode die je in Stripe/Mollie aanmaakt
+  betaalLink: "#",               // VERVANG door je Stripe- of Mollie-betaallink
+  premiumCodes: ["PREMIUM-VO
